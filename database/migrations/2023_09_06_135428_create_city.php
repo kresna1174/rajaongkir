f754eb1e31cb9226b1c9c88b6e733996bccc63a4
kresna <kresnacahyono7@gmail.com>
2023-09-06 21:45:29 +0700

    update: sprint 1

---
+++ database/migrations/2023_09_06_135428_create_city.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCity extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('city', function (Blueprint $table) {
+            $table->integer('city_id');
+            $table->integer('province_id');
+            $table->string('type');
+            $table->string('city_name');
+            $table->string('postal_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('city');
+    }
+}
---
+++ database/migrations/2023_09_06_135656_create_province.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProvince extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('province', function (Blueprint $table) {
+            $table->integer('province_id');
+            $table->string('province');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('province');
+    }
+}
